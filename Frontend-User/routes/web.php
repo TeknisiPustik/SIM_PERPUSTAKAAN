5e088edc61a88bafae640bb92b40171881966b5b
Risalsyah13 <134081250+Risalsyah13@users.noreply.github.com>
2023-06-20 23:21:31 +0700

    update forntend-user

--- Frontend-User/routes/web.php
+++ Frontend-User/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
